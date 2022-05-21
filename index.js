756d54cafc35202e5d9c9d0ba8a7eec58a0ab58b
Ievhenii <youn4ik@gmail.com>
2022-05-21 12:10:34 +0300

    added env

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const TelegramBot = require('node-telegram-bot-api');
 const bodyParser = require('body-parser');
 
